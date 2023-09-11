d18fa0c1fe597f331cd091be2552911db2cfadb0
Joel Sereno <jakayus123@gmail.com>
2023-09-10 21:00:28 -0700

    initial project setup

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Joel Sereno on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
