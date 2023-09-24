74c5e27fe2bca150603f5013a483005b54bbaaaa
Joel Sereno <jakayus123@gmail.com>
2023-09-24 16:18:23 -0700

    Add UserDefault variables to Onboarding
    UserDefault variables added for later use in the project.

--- LittleLemon/LittleLemon/Onboarding.swift
+++ LittleLemon/LittleLemon/Onboarding.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+// User Default keys
+
+let firstNameKey = "First_Name_Key"
+let lastNameKey = "Last_Name_Key"
+let emailKey = "Email_Key"
+
+
 struct Onboarding: View {
     
     @State private var firstName = ""
@@ -18,6 +25,20 @@ struct Onboarding: View {
             TextField("First Name", text: $firstName)
             TextField("Last Name", text: $lastName)
             TextField("Email", text: $email)
+            Button( action: {
+                
+                // store values in UserDefaults
+                if firstNameKey.isEmpty {
+                    UserDefaults.standard.set(firstName, forKey: firstNameKey)
+                } else if lastNameKey.isEmpty {
+                    UserDefaults.standard.set(lastName, forKey: lastNameKey)
+                } else if emailKey.isEmpty {
+                    // TODO: add email validation
+                    UserDefaults.standard.set(email, forKey: emailKey)
+                }
+            }, label: {
+                Text("Register")
+            })
         }
     }
 }
