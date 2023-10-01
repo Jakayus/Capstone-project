349388c07814726ab6485a06c36fc9a774404f66
Joel Sereno <jakayus123@gmail.com>
2023-09-30 17:34:24 -0700

    Adjust Hero Section UI
    Adjust the "Hero" section of the UI, which consists of an image and App name and description.
    
    New assets added to support the UI updates.

--- LittleLemon/LittleLemon/Menu.swift
+++ LittleLemon/LittleLemon/Menu.swift
@@ -12,12 +12,44 @@ struct Menu: View {
     // Sample data until real data is acquired
     private let TemporarySamples = ["Burger", "Fries", "Drink"]
     
+    let AppDescription = "We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist."
+    
     var body: some View {
         VStack{
-            // TODO: Provide descriptions and enhance UI
-            Text("TODO: App Name")
-            Text("TODO: App Location")
-            Text("TODO: App Description")
+            Image("Logo")
+                .resizable()
+                .fixedSize()
+            
+            VStack (alignment: .leading) {
+                VStack (alignment: .leading) {
+                    Text("Litte Lemon").foregroundColor(Color("Primary2"))
+                        .font(.largeTitle)
+                        .font(.custom("Avenir Book", size: 22))
+                        .bold()
+                        .padding([.leading])
+                    Text("Chicago")
+                        .foregroundColor(Color.white)
+                        .font(.title)
+                        .padding([.leading])
+                }
+
+                HStack{
+                    Text(AppDescription)
+                        .foregroundColor(Color.white)
+                        .font(.title3)
+                        .padding([.leading, .trailing])
+                    Image("Hero Image")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(10)
+                        .padding([.bottom, .trailing])
+                }
+            }
+            .background(Color("Primary1"))
+            .ignoresSafeArea()
+            
+            
+            
             List {
                 ForEach(TemporarySamples, id: \.self) { foodItem in
                     Text(foodItem)
