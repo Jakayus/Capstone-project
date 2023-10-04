2db7a2f4e27d1a288f2718c63ea3811cc4fdcfcf
Joel Sereno <jakayus123@gmail.com>
2023-10-03 20:33:48 -0700

    Create proof of concept for item details
    Create a proof of concept for item details view.

--- LittleLemon/LittleLemon/MenuItemDetails.swift
+++ LittleLemon/LittleLemon/MenuItemDetails.swift
@@ -8,18 +8,41 @@
 import SwiftUI
 
 struct MenuItemDetails: View {
-    let dishEntity: Dish
+    //let dishEntity: Dish
     
     var body: some View {
-        VStack {
-            Text("Destination Reached")
-            Text("\(dishEntity.foodDescription ?? "Unknown food")")
+        ZStack {
+            VStack {
+                HStack{
+                    Text("Pasta")
+                        .font(.system(size: 60))
+                        .bold()
+                        .padding([.leading])
+                    Spacer()
+                    Text("$12.99")
+                        .font(.title2)
+                        .bold()
+                        .padding([.trailing])
+                }
+                Image("Pasta")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(5)
+                    .padding()
+                Text("Penne with fried aubergines, cherry tomatoes, tomato sauce, fresh chilli, garlic, basil & salted ricotta cheese.")
+                    .font(.title3)
+                    .italic()
+                    .foregroundColor(Color.gray)
+                    .padding()
+                    
+                
+            } // end VStack
         }
     }
 }
 
-//struct MenuItemDetails_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MenuItemDetails()
-//    }
-//}
+struct MenuItemDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuItemDetails()
+    }
+}
